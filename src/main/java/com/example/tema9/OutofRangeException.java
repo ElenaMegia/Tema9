5753ca683906269f0eadc6ce8996d075d225c4bb
ElenaMegia <emegican@myuax.com>
2023-03-18 17:37:57 +0100

    Punto c terminado

---
+++ src/main/java/com/example/tema9/OutofRangeException.java
@@ -0,0 +1,7 @@
+package com.example.tema9;
+
+public class OutofRangeException extends Throwable {
+    public OutofRangeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/tema9/ejercicio1/Ejercicio1.java
@@ -0,0 +1,22 @@
+package com.example.tema9.ejercicio1;
+
+public class Ejercicio1 {
+    Bicicleta b1 = new Bicicleta("Lapierre", "speed 400", 1, 27);
+    Bicicleta b2 = new Bicicleta("Btwin", "riverside 900", 0, 10);
+    Giroscopio g1 = new Giroscopio("Segway", "Nine", 0, 40);
+    Giroscopio g2 = new Giroscopio("Weebot", "Echo", 1, 35);
+    Segway s1 = new Segway("Immotion", "v8", 0, 40);
+    Segway s2 = new Segway("Segway", "Ninebot One E+", 0, 30);
+
+
+    public void Ejercicio1metodo() {
+        b1.mostrar();
+        b2.mostrar();
+        g1.mostrar();
+        g2.mostrar();
+        s1.mostrar();
+        s2.mostrar();
+
+
+    }
+}
